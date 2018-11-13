523d766ee7e09d2ce5b7b3a35c078f8226e26c73
Jonathan Whittle <jonathan.m.whittle@gmail.com>
2018-11-12 20:30:36 -0500

    update Macros.js calulations

--- src/components/Macros.js
+++ src/components/Macros.js
@@ -12,15 +12,29 @@ class Macros extends Component {
         const { age, weight, height, activity, fitnessSelection, mealsSelection, genderSelection, next } = this.props
 
         const maleBmr = Math.round((66 + (6.3 * weight) + (12.9 * height) - (6.8 * age)) * activity)
-
         const femaleBmr = Math.round((655 + (4.3 * weight) + (4.7 * height) - (4.7 * age)) * activity)
-
         let gender = (genderSelection.male === true) ? 'Male' :                             (genderSelection.female === true) ? 'Female' : ""
-
         let bmr = (genderSelection.male === true) ? maleBmr :                              (genderSelection.female === true) ? femaleBmr : ""
-
         const buildBmr =  bmr + 500
         const burnBmr = bmr - 500
+
+        let calulatedProteinCarbs =
+            Math.round((!fitnessSelection.selection) ? 0 :
+            (fitnessSelection.selection === 'build') ?
+                ((buildBmr * 0.4) / 4) :
+            (fitnessSelection.selection === 'maintain') ?
+                ((bmr * 0.4) / 4) :
+            (fitnessSelection.selection === 'burn') ?
+                ((burnBmr * 0.4) / 4) : 0)
+
+        let calulatedFat =
+            Math.round((!fitnessSelection.selection) ? 0 :
+            (fitnessSelection.selection === 'build') ?
+                ((buildBmr * 0.4) / 4) :
+            (fitnessSelection.selection === 'maintain') ?
+                ((bmr * 0.4) / 4) :
+            (fitnessSelection.selection === 'burn') ?
+                ((burnBmr * 0.2) / 9) : 0)
 
         return (
         <div className="macros-table" id="tracker">
@@ -59,14 +73,14 @@ class Macros extends Component {
                     <tr>
                         <td className="center">Fitness Goal</td>
                         <td className="center">
-                            {fitnessSelection.selection ?
-                            fitnessSelection.selection : ''}</td>
+                            { fitnessSelection.selection ?
+                            fitnessSelection.selection : '' }</td>
                     </tr>
                     <tr>
                         <td className="center">Meals/Day</td>
                         <td className="center">
-                            {mealsSelection.selection ?
-                             mealsSelection.selection : ''}</td>
+                            { mealsSelection.selection ?
+                             mealsSelection.selection : '' }</td>
                     </tr>
                 </tbody>
             </table>
@@ -81,40 +95,28 @@ class Macros extends Component {
                 <tbody>
                     <tr>
                         <td className="center">
-                        {
-                            Math.round((!fitnessSelection.selection) ? 0 :
-                            (fitnessSelection.selection === 'build') ?
-                            ((buildBmr * 0.4) / 4) :
-                            (fitnessSelection.selection === 'maintain') ?
-                            ((bmr * 0.4) / 4) :
-                            (fitnessSelection.selection === 'burn') ?
-                            ((burnBmr * 0.4) / 4) : 0)
-                        } grams / day</td>
+                        { calulatedProteinCarbs } grams / day</td>
                         <td className="center">
-                        {
-                            Math.round((!fitnessSelection.selection) ? 0 :
-                            (fitnessSelection.selection === 'build') ?
-                            ((buildBmr * 0.4) / 4) :
-                            (fitnessSelection.selection === 'maintain') ?
-                            ((bmr * 0.4) / 4) :
-                            (fitnessSelection.selection === 'burn') ?
-                            ((burnBmr * 0.4) / 4) : 0)
-                        } grams / day</td>
+                        { calulatedProteinCarbs } grams / day</td>
                         <td className="center">
-                        {
-                            Math.round((!fitnessSelection.selection) ? 0 :
-                            (fitnessSelection.selection === 'build') ?
-                            ((buildBmr * 0.2) / 9) :
-                            (fitnessSelection.selection === 'maintain') ?
-                            ((bmr * 0.2) / 9) :
-                            (fitnessSelection.selection === 'burn') ?
-                            ((burnBmr * 0.2) / 9) : 0)
-                        } grams / day</td>
+                        { calulatedFat } grams / day</td>
                     </tr>
                     <tr>
-                        <td className="center"><b>12 oz</b> Chicken</td>
-                        <td className="center"><b>1 cup</b> Brown Rice</td>
-                        <td className="center"><b>1 handful</b> Cashews</td>
+                        <td className="center"> {
+                            mealsSelection.selection ?
+                            calulatedProteinCarbs / mealsSelection.selection
+                            : 0
+                            } grams / meal</td>
+                        <td className="center"> {
+                            mealsSelection.selection ?
+                            calulatedProteinCarbs / mealsSelection.selection
+                            : 0
+                            } grams / meal</td>
+                        <td className="center"> {
+                            mealsSelection.selection ?
+                            calulatedFat / mealsSelection.selection
+                            : 0
+                            } grams / meal</td>
                     </tr>
                     <tr>
                         <td className="center"><b>3 large</b> Eggs</td>
